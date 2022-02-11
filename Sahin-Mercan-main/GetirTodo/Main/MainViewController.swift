e3e86a7c04fae8ac4b60f91b70db4589d6eb03a6
sahinmercan <sahinmrcn@gmail.com>
2022-02-11 10:17:03 +0300

    Main ekrani olusturuldu.

---
+++ Sahin-Mercan-main/GetirTodo/Main/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  GetirTodo
+//
+//  Created by sahin on 11.02.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+    }
+    
+
+    func setup() {
+        
+    }
+}
